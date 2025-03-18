de9c233dedeac9185dfeafd8b7779209ba78c0a1
Huriyah063 <hurihuriyahhuri@gmail.com>
2025-03-18 21:50:20 +0700

    Add files via upload

---
+++ pertemuan3/admin/sidebar.php
@@ -0,0 +1,9 @@
+<div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
+                <div class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil.php">HASIL UTS </a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil2.php">Shortcuts</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil3.php">Overview</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil4.php">Events</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil5.php">Profile</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="panggil6.php">Status</a>
+                </div>
